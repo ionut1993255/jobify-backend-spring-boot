00d374c7ee73df485c65a4bca9edef359fd62db2
ionut <ionut_ilisei@yahoo.com>
2025-05-25 20:51:08 +0300

    Initial commit

---
+++ src/main/java/dev/ionut/jobify/JobifyApplication.java
@@ -0,0 +1,12 @@
+package dev.ionut.jobify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobifyApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/ionut/jobify/JobifyApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.ionut.jobify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
