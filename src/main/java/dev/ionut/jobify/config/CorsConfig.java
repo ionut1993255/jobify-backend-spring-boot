ab0cc577148325a31096cf4a1206991625df57cb
ionut <ionut_ilisei@yahoo.com>
2025-05-26 18:10:06 +0300

    chore(config): configure CORS and ModelMapper beans

---
+++ src/main/java/dev/ionut/jobify/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package dev.ionut.jobify.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry corsRegistry) {
+                corsRegistry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
